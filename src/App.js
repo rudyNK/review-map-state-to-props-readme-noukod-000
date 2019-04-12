68026a48fac5ba14c9c65261c790f0a66b9a3062
Rudy Ravelin <rudy@noukod.com>
2019-04-12 13:56:57 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
- 
- 
+
+
 class App extends Component {
- 
+
   handleOnClickItems() {
     this.props.store.dispatch({
       type: 'GET_COUNT_OF_ITEMS',
     });
   }
- 
+
   handleOnClickUsers() {
     this.props.store.dispatch({
       type: 'GET_COUNT_OF_USERS',
     })
   }
- 
+
   render() {
     debugger;
     return (
@@ -32,10 +32,10 @@ class App extends Component {
     );
   }
 }
- 
+
 const mapStateToProps = (state) => {
   // debugger;
   return { items: state.items }
 }
- 
+
 export default connect(mapStateToProps)(App);
